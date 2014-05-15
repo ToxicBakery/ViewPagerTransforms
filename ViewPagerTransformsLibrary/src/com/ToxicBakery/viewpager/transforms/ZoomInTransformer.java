2327901fe90573bb3eb59097fabbeb4929063b30
Ian Thomas <toxicbakery@gmail.com>
2014-05-14 21:02:37 -0400

    #4 Fixed zoom in and zoom out animations

--- ViewPagerTransformsLibrary/src/com/ToxicBakery/viewpager/transforms/ZoomInTransformer.java
+++ ViewPagerTransformsLibrary/src/com/ToxicBakery/viewpager/transforms/ZoomInTransformer.java
@@ -6,12 +6,12 @@ public class ZoomInTransformer extends ABaseTransformer {
 
 	@Override
 	protected void onTransform(View view, float position) {
-		final float scale = Math.abs(position);
+		final float scale = position < 0 ? position + 1f : Math.abs(1f - position);
 		view.setScaleX(scale);
 		view.setScaleY(scale);
 		view.setPivotX(view.getWidth() * 0.5f);
 		view.setPivotY(view.getHeight() * 0.5f);
-		view.setAlpha(position < -1f || position > 1f ? 0 : 1f - (scale - 1f));
+		view.setAlpha(position < -1f || position > 1f ? 0f : 1f - (scale - 1f));
 	}
 
 }
--- ViewPagerTransformsLibrary/src/com/ToxicBakery/viewpager/transforms/ZoomOutTranformer.java
+++ ViewPagerTransformsLibrary/src/com/ToxicBakery/viewpager/transforms/ZoomOutTranformer.java
@@ -6,12 +6,12 @@ public class ZoomOutTranformer extends ABaseTransformer {
 
 	@Override
 	protected void onTransform(View view, float position) {
-		final float scale = 2f - Math.abs(position);
+		final float scale = 1f + Math.abs(position);
 		view.setScaleX(scale);
 		view.setScaleY(scale);
 		view.setPivotX(view.getWidth() * 0.5f);
 		view.setPivotY(view.getHeight() * 0.5f);
-		view.setAlpha(position < -1f || position > 1f ? 0 : 1f - (scale - 1f));
+		view.setAlpha(position < -1f || position > 1f ? 0f : 1f - (scale - 1f));
 	}
 
 }
