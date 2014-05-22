1119cc0f6653ad93c646c053fd2d7ea966e6a91d
Ian Thomas <toxicbakery@gmail.com>
2014-05-21 22:23:43 -0400

    #10 Fixed RotateDownTransformer - RotateDownTransformer no longer clips on left to right swipes in portrait mode.

--- ViewPagerTransformsLibrary/src/com/ToxicBakery/viewpager/transforms/RotateDownTransformer.java
+++ ViewPagerTransformsLibrary/src/com/ToxicBakery/viewpager/transforms/RotateDownTransformer.java
@@ -9,10 +9,11 @@ public class RotateDownTransformer extends ABaseTransformer {
 	@Override
 	protected void onTransform(View view, float position) {
 		final float width = view.getWidth();
+		final float height = view.getHeight();
 		final float rotation = ROT_MOD * position * -1.25f;
 
 		view.setPivotX(width * 0.5f);
-		view.setPivotY(width);
+		view.setPivotY(height);
 		view.setRotation(rotation);
 	}
 	
