f43fb3bd6153709d7e97c382bfff9fc4264d4344
ZhangLiang <zhangliang@vilstart.com>
2016-06-29 12:49:23 +0800

    resolve problem: new page can't handle click event!

--- library/src/main/java/com/ToxicBakery/viewpager/transforms/FlipHorizontalTransformer.java
+++ library/src/main/java/com/ToxicBakery/viewpager/transforms/FlipHorizontalTransformer.java
@@ -20,14 +20,25 @@ import android.view.View;
 
 public class FlipHorizontalTransformer extends ABaseTransformer {
 
-	@Override
-	protected void onTransform(View view, float position) {
-		final float rotation = 180f * position;
+    @Override
+    protected void onTransform(View view, float position) {
+        final float rotation = 180f * position;
 
-		view.setAlpha(rotation > 90f || rotation < -90f ? 0 : 1);
-		view.setPivotX(view.getWidth() * 0.5f);
-		view.setPivotY(view.getHeight() * 0.5f);
-		view.setRotationY(rotation);
-	}
+        view.setAlpha(rotation > 90f || rotation < -90f ? 0 : 1);
+        view.setPivotX(view.getWidth() * 0.5f);
+        view.setPivotY(view.getHeight() * 0.5f);
+        view.setRotationY(rotation);
+    }
 
+    @Override
+    protected void onPostTransform(View page, float position) {
+        super.onPostTransform(page, position);
+
+        //resolve problem: new page can't handle click event!
+        if (position > -0.5f && position < 0.5f) {
+            page.setVisibility(View.VISIBLE);
+        } else {
+            page.setVisibility(View.INVISIBLE);
+        }
+    }
 }
